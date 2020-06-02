b40c9e99aeaa6850b7eb5c5f6d2c10a7adce988d
fabioMatosCosta <fabiodanielmc@gmail.com>
2020-06-02 15:37:44 +0200

    create sqlite db and migrate

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/person', function(){
+    $person = [
+        'first_name' => 'John',
+        'last_name' => 'Doe'
+    ];
+    return $person;     
 });
