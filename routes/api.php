83cfd2be6db84952386f03ad86e96c62bcca253e
fabioMatosCosta <fabiodanielmc@gmail.com>
2020-06-03 16:45:45 +0200

    first versioning setup

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,6 @@ use App\Person;
 |
 */
 
-Route::apiResource('/person', 'PersonController');
+// Route::prefix('v1')->group(function(){
+    Route::apiResource('/person', 'Api\v1\PersonController');
+// });
